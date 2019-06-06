18924a0022f1606a6d533ca9790061ded9a6aae0
caizhili <caizhili@corp.netease.com>
2019-06-06 22:23:20 +0800

    up

--- app/win32/win32_handmade.cpp
+++ app/win32/win32_handmade.cpp
@@ -6,6 +6,19 @@
 
 #include <windows.h>
 
+typedef struct tagWNDCLASSA {
+    UINT      style;
+    WNDPROC   lpfnWndProc;
+    int       cbClsExtra;
+    int       cbWndExtra;
+    HINSTANCE hInstance;
+    HICON     hIcon;
+    HCURSOR   hCursor;
+    HBRUSH    hbrBackground;
+    LPCSTR    lpszMenuName;
+    LPCSTR    lpszClassName;
+} WNDCLASSA, *PWNDCLASSA, *NPWNDCLASSA, *LPWNDCLASSA;
+
 int CALLBACK WinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPSTR lpCmdLine,
